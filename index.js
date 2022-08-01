98f0996e81460b43984719c704fb3bfded27105a
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2022-08-01 22:39:36 +0200

    added all functionality

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const Fighter = require("./src/Fighter.js");
+const Weapon = require("./src/Weapon.js");
+const Shield = require("./src/Shield.js");
+
 
 /** Create Heracles  */
 const heracles = new Fighter("🧔 Heracles", 20, 6);
+const sword = new Weapon("Sword", 10);
+heracles.weapon = sword;
+const shield = new Shield("Shield", 10);
+heracles.shield = shield;
 
 /** Create the opponent  */
 const boar = new Fighter("🐗 Erymanthian Boar", 25, 12);
@@ -25,3 +32,25 @@ const score = (fighter1, fighter2) => {
     loser: fighter1
   };
 };
+
+// Let's fight !!!
+{
+  let round = 0;
+
+  while (heracles.isAlive() && boar.isAlive()) {
+      console.log(`🕛 Round #${round}`);
+
+      heracles.fight(boar);
+      console.log(roundDisplay(heracles, boar));
+
+      boar.fight(heracles);
+      console.log(roundDisplay(boar, heracles));
+
+      round++;
+  }
+
+  const result = score(heracles, boar);
+
+  console.log(`💀 ${result.loser.name} is dead`);
+  console.log(`🏆 ${result.winner.name} wins (💙 ${result.winner.life} )`);
+}
--- src/Fighter.js
+++ src/Fighter.js
@@ -6,14 +6,16 @@ class Fighter {
         this.strength = strength;
         this.dexterity = dexterity;
         this.life = MAX_LIFE;
+        this.weapon = null;
+        this.shield = null;
     }
 
 
     // Launch a fight
     fight(defender) {
-        const attackPoints = this.getRandomInt(this.strength);
+        const attackPoints = this.getRandomInt(this.getDamage());
 
-        const damages = Math.max(attackPoints - defender.dexterity, 0);
+        const damages = Math.max(attackPoints - this.getDefense(), 0);
 
         defender.life = Math.max(defender.life - damages, 0);
     }
@@ -28,6 +30,14 @@ class Fighter {
     // Determine if a fighter is still alive
     isAlive() {
         return this.life > 0;
+    }
+
+    getDamage() {
+        return this.strength + (this.weapon === null ? 0 : this.weapon.damage);
+    }
+
+    getDefense() {
+        return this.dexterity + (this.shield === null ? 0 : this.shield.protection);
     }
 }
 
